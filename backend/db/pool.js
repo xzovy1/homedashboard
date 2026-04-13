4460898bebd7eb41fa99fd7a889ab0493db93d81
cabelstein12 <Caleb.Steinwandt@gmail.com>
2026-04-12 21:33:21 -0600

    update for local development

--- backend/db/pool.js
+++ backend/db/pool.js
@@ -1,14 +1,19 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-// const databaseUrl =
-//   process.env.NODE_ENV === "test"
-//     ? process.env.TEST_DB_URL
-//     : process.env.DB_URL;
-module.exports = new Pool({
+const testPool = new Pool({
+  port: process.env.TEST_DB_PORT,
+  user: process.env.TEST_DB_USER,
+  password: process.env.TEST_DB_PASSWORD,
+  database: process.env.TEST_DB_NAME,
+});
+
+const pool = new Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 });
+
+module.exports = process.env.NODE_ENV === "test" || "dev" ? testPool : pool;
