f0628d6f493a67eea0f7ab19a53261df3d365bc0
cabelstein12 <Caleb.Steinwandt@gmail.com>
2026-08-16 19:51:59 -0600

    update offline last will message and shorten duration for sleep on error

--- sensors/office_satellite/platformio/src/main.cpp
+++ sensors/office_satellite/platformio/src/main.cpp
@@ -16,6 +16,7 @@ const int orangeLed = 14; // indicates sensor fault
 // Deep Sleep
 #define TIME_TO_SLEEP   20 * 60     // Time ESP32 will sleep (in seconds)
 #define uS_TO_S_FACTOR  1000000ULL  // Conversion factor for micro seconds
+#define SLEEP_DURATION TIME_TO_SLEEP * uS_TO_S_FACTOR
 
 DHT dht(DHT_PIN, DHTTYPE);
 WiFiClient espClient;
@@ -24,9 +25,9 @@ PubSubClient client(espClient);
 //MQTT
 const char* topic = "home/office/sensors";
 const char* willTopic = "home/office/sensors/lastwill";
-const char* willMsg = "offline";
+const char* willMsg = "office offline";
 
-void espSleep();
+void espSleep(unsigned long duration);
 
 void setup() {
   Serial.begin(115200);
@@ -51,72 +52,76 @@ void setup() {
     Serial.print(".");
     attempts++;
   }
-  //specify sleep timer
-  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
 
   if (WiFi.status() != WL_CONNECTED) {
     Serial.println("\nWiFi Connection Failed. Going back to sleep.");
     // turn yellow on, orange off if connection failed 
     digitalWrite(yellowLed, HIGH);
-    espSleep();
+    //specify sleep timer
+    //shorter sleep for wifi fail
+    espSleep(SLEEP_DURATION / 5);
   }else{
     digitalWrite(yellowLed, LOW);
   }
-  
+
   Serial.println("\nWiFi Connected.");
 
   // Configure & Connect MQTT
   client.setServer(BROKER_IP, MQTT_PORT);
   Serial.print("Connecting to MQTT Broker...");
   
-  String clientId = "ESP32Client-" + String(random(0, 0xffff), HEX);
-  if (client.connect(clientId.c_str(), NULL, NULL, willTopic, 1, true, willMsg)) {
+  String clientId = "ESP32Client-Office";
+  if (client.connect(clientId.c_str(), willTopic, 1, true, willMsg)) {
+    client.publish(willTopic, "office online", true);
     Serial.println("Connected.");
     digitalWrite(yellowLed, LOW);
+
+    //delay for sensors
+    delay(2000);
+    // Read Sensor 
+    float humidity = dht.readHumidity();
+    float temperature = dht.readTemperature();
+
+    if (isnan(humidity) || isnan(temperature)) {
+      Serial.println("Failed to read from DHT sensor!");
+      //turn on orange led if sensor failed.
+      digitalWrite(orangeLed, HIGH);
+    } else {
+      // Publish Payload to MQTT
+      String payload = "{\"temperature\":" + String(temperature, 2) + 
+                      ",\"humidity\":" + String(humidity, 2) + "}";
+      
+      Serial.print("Publishing payload: ");
+      Serial.println(payload);
+      //cast payload string to uint8 to read the payload length
+      client.publish(topic, (const uint8_t*)payload.c_str(), payload.length(), true);
+      delay(500);
+      client.disconnect();
+      digitalWrite(orangeLed, LOW);
+    }
   } else {
-    // turn yellow on if connection failed 
+    // turn yellow led on if connection failed 
     digitalWrite(yellowLed, HIGH);
     Serial.print("Failed, rc=");
     Serial.print(client.state());
-    espSleep();
+    // shorter sleep for mqtt fail
+    espSleep(SLEEP_DURATION / 5);
   }
-
-  // Read Sensor 
-  float humidity = dht.readHumidity();
-  float temperature = dht.readTemperature();
-
-  if (isnan(humidity) || isnan(temperature)) {
-    Serial.println("Failed to read from DHT sensor!");
-    //turn on orange led on sensor fail.
-    digitalWrite(orangeLed, HIGH);
-  } else {
-    // Publish Payload to MQTT
-    String payload = "{\"temperature\":" + String(temperature, 2) + 
-                     ",\"humidity\":" + String(humidity, 2) + "}";
-    
-    Serial.print("Publishing payload: ");
-    Serial.println(payload);
-    //cast payload string to uint8 to read the payload length
-    client.publish(topic, (const uint8_t*)payload.c_str(), payload.length(), true);
-    client.disconnect();
-    digitalWrite(orangeLed, LOW);
-  }
-
-  // Small delay to ensure MQTT buffer
-  delay(500); 
   
   // Start Deep Sleep
-  espSleep();
+  espSleep(SLEEP_DURATION);
 }
 
 void loop() {
 
 }
 
-void espSleep(){
+void espSleep(unsigned long duration){
   Serial.println(" Going back to sleep.");
+  esp_sleep_enable_timer_wakeup(duration);
   gpio_hold_en((gpio_num_t)orangeLed);
   gpio_hold_en((gpio_num_t)yellowLed);
   gpio_deep_sleep_hold_en();
+  delay(500);
   esp_deep_sleep_start();
 }
